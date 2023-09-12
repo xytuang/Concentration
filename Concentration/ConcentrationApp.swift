b21187ee43e2d065cf8eff957bc85e7d6ca0cbb5
Xiang Yu Tuang <xiangyutuang@gmail.com>
2023-09-12 13:53:16 -0400

    Initial Commit

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by Xiang Yu Tuang on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
