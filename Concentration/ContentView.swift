49f58c318879b81b03e10d3f250c7db1227dfe92
Xiang Yu Tuang <xiangyutuang@gmail.com>
2023-09-13 13:33:28 -0400

    Togglable isFaceUp

--- Concentration/ContentView.swift
+++ Concentration/ContentView.swift
@@ -22,20 +22,22 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = false
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("😀")
             }
             else{
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
             
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
