a29e26becaa22e97a84a3240da4876d5cca7ff8a
Xiang Yu Tuang <xiangyutuang@gmail.com>
2023-09-13 12:59:59 -0400

    First Commit

--- Concentration/ContentView.swift
+++ Concentration/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
+        .imageScale(.large)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("😀")
+            }
+            else{
+                RoundedRectangle(cornerRadius: 12)
+            }
+            
+        }
     }
 }
 
